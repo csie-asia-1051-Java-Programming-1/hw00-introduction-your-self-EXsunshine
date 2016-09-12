79e958ae5b91696eec0971ec48f1ee66e85c95d3
EXsunshine <105021057@live.asia.edu.tw>
2016-09-12 15:45:47 +0800

    hw00-提交
    開學第一天

---
+++ test/src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("我是張岳埕");
+System.out.println("學號:105021057");
+System.out.println("OuOb");
+	}
+
+}
